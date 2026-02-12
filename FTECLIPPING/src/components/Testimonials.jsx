6e92a7bcfb348e7ea0b976b854f6478446e7828c
pruthvirajbhuruk <108937938+pruthvirajbhuruk@users.noreply.github.com>
2026-02-12 05:57:17 +0000

    all

--- FTECLIPPING/src/components/Testimonials.jsx
+++ FTECLIPPING/src/components/Testimonials.jsx
@@ -9,17 +9,14 @@ export default function Testimonials() {
           <div className="quote">“</div>
           <p className="text">
             Every week another agency like yours reaches out to see if I want
-            them to make shorts for me. I'm always like, absolutely not. Atomik
-            Growth makes shorts that are 1000x better than anything you could
+            them to make shorts for me. I'm always like, absolutely not. FTE makes shorts that are 1000x better than anything you could
             do. Thank you. You rock.
           </p>
 
           <div className="author">
-            <img src="/avatar1.jpg" alt="Jack Rhysider" />
             <div>
-              <h4>Jack Rhysider</h4>
-              <span>Darknet Diaries</span>
-            </div>
+              <h4>Ryan Holland</h4>
+              </div>
           </div>
         </div>
 
@@ -27,27 +24,24 @@ export default function Testimonials() {
         <div className="right-column">
           <div className="testimonial-card">
             <div className="quote">“</div>
-            <p className="text">This is awesome! Great to see.</p>
+            <p className="text">I was first not sure if this would work for me but the results proved me wrong for sure.</p>
 
             <div className="author">
-              <img src="/avatar2.jpg" alt="Jason Shuman" />
               <div>
-                <h4>Jason Shuman</h4>
-                <span>Primary VC</span>
+                <h4>Amit Kulkarni</h4>
+                
               </div>
             </div>
           </div>
 
           <div className="testimonial-card">
             <div className="quote">“</div>
-            <p className="text">Glanced at the trailer and loved it :)</p>
+            <p className="text">I just started and i am already loving this :)</p>
 
             <div className="author">
-              <img src="/avatar3.jpg" alt="Guy Podjarny" />
-              <div>
-                <h4>Guy Podjarny</h4>
-                <span>Tessl</span>
-              </div>
+            <div>
+                <h4>Gauri Rao </h4>
+                </div>
             </div>
           </div>
         </div>
